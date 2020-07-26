42a09c108a715ccb3d174210507187f0700a356d
mucahit <mucahitpa@gmail.com>
2020-07-26 11:34:11 -0400

    added

--- projects/002-milliseconds-converter/app.py
+++ projects/002-milliseconds-converter/app.py
@@ -4,29 +4,13 @@ developer_name = 'Mucahit Koca'
 
 def converter(num):
     times = {"second": 1000, "minute": 60000, "hour": 3600000}
-    if num < 1000:
-        result = f"just {num} milisecond/s"
-        return result
-    else:
-        hour = num // times["hour"]
-        num -= hour * times["hour"]
-        minute = num // times["minute"]
-        num -= minute * times["minute"]
-        second = num // times["second"] 
-        if hour == 0:
-            hour = "" 
-        else:
-            hour = str(hour) + " hour/s "  
-        if minute == 0:
-            minute = "" 
-        else:
-            minute = str(minute) + " minute/s "
-        if second == 0:
-            second = "" 
-        else:
-            second = str(second) + " second/s"
-        result = f"{hour}{minute}{second}" 
-        return result
+    hour = num // times["hour"]
+    num -= hour * times["hour"]
+    minute = num // times["minute"]
+    num -= minute * times["minute"]
+    second = num // times["second"] 
+    result = f"{hour} hour/s " * (hour!=0) + f"{minute} minute/s " * (minute!=0) + f"{second} second/s" * (second!=0) or f"just {num} millisecond/s" 
+    return result
 
 app = Flask(__name__)
 
